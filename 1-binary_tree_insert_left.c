d1d96a666f2757ccf1e823e16ed8f192bcc55b0b
maramilod <marammilod03@gmail.com>
2024-01-04 20:39:17 +0000

    add

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,7 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left)
 	{
 		newn->left = parent->left;
-		parent->left->parent = newn
+		parent->left->parent = newn;
 	}
 	parent->left = newn;
 	return (newn);
